157ad68f5526b8833ba8f1b50fa11a9f3e7008f0
Premiummalts <minjae2717@gmail.com>
2022-05-26 23:07:51 +0900

    홀수만 더하기 2커밋 scanner를 이용하여 입력값을 받아 홀수만을 더해 출력하는 코드작성

--- JAVA d1/AddOnlyOddNumbers.java
+++ JAVA d1/AddOnlyOddNumbers.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
+
 public class AddOnlyOddNumbers {
-  int i;
-  public static void main(String[] args) {
+  
+
+  public static void addonlyoddnumbers() {
+    Scanner sc = new Scanner(System.in);
+    int b[] = new int[10];
+    int result = 0;
+    for (int i = 0; i < 10; i++) {
       
-      System.out.println();
+      b[i] = sc.nextInt();
+    }
+    sc.close();
+     
+    System.out.println(b);
+
+    for (int i = 0; i < b.length; i++) {
+      if (b[i] % 2 == 1) {
+        result += b[i];
+      }
+    }
+    System.out.println(result);
 
   }
+  public static void main(String[] args) {
+  addonlyoddnumbers();
+  }
 }
+
