3280b628f243bc47ff5a91faf51e4a09a04469ac
KIM MINJAE <minjae2717@gmail.com>
2022-06-26 23:40:08 +0900

    테스트케이스를 입력받고 숫자 10개를 입력 받아 그 수들의 평균을 구한다. (소수점 첫째자리에서 반올림 코드 미완성)

---
+++ JAVA d1/GetAvg.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class GetAvg {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int[] arr = new int[10];
+    int count = sc.nextInt();
+    int sum = 0;
+    int avg = 0;
+    for (int k = 1; k < count+1; k++) {
+
+      for (int i = 0; i < 10; i++) {
+        int a = sc.nextInt();
+        if (a >= 0 && a <= 1000) {
+          arr[i] = a;
+          // System.out.println(i+1+"번째");
+        }
+      }
+      for (int j = 0; j < arr.length; j++) {
+        sum += arr[j];
+        avg = sum / arr.length;
+      }
+      System.out.print("#"+k + " ");
+      System.out.println(avg);
+      sum = 0;
+    }
+    // System.out.println("횟수:"+count);
+    sc.close();
+  }
+}
